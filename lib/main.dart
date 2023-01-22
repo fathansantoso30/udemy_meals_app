8e3d7f5ce595b92ef919f0ad9a42b0eba3ada614
fathansantoso30 <fathansantoso30@gmail.com>
2023-01-22 14:07:31 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_meals_app/categories_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CategoriesScreen(),
+    );
+  }
+}
