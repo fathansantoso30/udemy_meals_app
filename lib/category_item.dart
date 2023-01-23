4b7dce3dc696078473806dc3d950fa7a2b99902e
fathansantoso30 <fathansantoso30@gmail.com>
2023-01-23 22:42:04 +0700

    added meals screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_meals_app/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -6,24 +7,33 @@ class CategoryItem extends StatelessWidget {
 
   const CategoryItem({super.key, required this.title, required this.color});
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx)
+        .push(MaterialPageRoute(builder: (_) => const CategoryMealsScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
+    return InkWell(
+      splashColor: Theme.of(context).primaryColor,
+      onTap: () => selectCategory(context),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+          ),
         ),
-      ),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.titleMedium,
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
       ),
     );
   }
