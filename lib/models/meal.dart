a35be686eca56b8720d7a4d41e8e6c73562b0023
fathansantoso30 <fathansantoso30@gmail.com>
2023-01-24 11:44:13 +0700

    added meal models

---
+++ lib/models/meal.dart
@@ -0,0 +1,42 @@
+enum Complexity {
+  simple,
+  challenging,
+  hard,
+}
+
+enum Affordability {
+  affordable,
+  pricey,
+  luxurious,
+}
+
+class Meal {
+  final String id;
+  final String title;
+  final List<String> categories;
+  final String imageUrl;
+  final List<String> ingredients;
+  final List<String> steps;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
+
+  Meal(
+      this.id,
+      this.title,
+      this.categories,
+      this.imageUrl,
+      this.ingredients,
+      this.steps,
+      this.duration,
+      this.complexity,
+      this.affordability,
+      this.isGlutenFree,
+      this.isLactoseFree,
+      this.isVegan,
+      this.isVegetarian);
+}
